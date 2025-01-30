c62cecbb55f66f926c3b4eca2d74d0e2f0e5ed60
piki <vickyaldiano5652@gmail.com>
2025-01-30 19:35:31 +0700

    matiin klik kanan + text selecting

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -168,6 +168,7 @@
         </div>
       </div>
     </div>
+    <script src="{{ asset('js/script.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.js"></script>
     <script>
